2e2bf4bb38f46d13754157707c5b03a2ab73a41f
niko <nicovanommen.nvo@gmail.com>
2024-01-26 14:23:11 +0000

    Added I2C functionality for sensor

--- src/ti_lidar_sensor.py
+++ src/ti_lidar_sensor.py
@@ -1,31 +1,78 @@
-
-
+import rospy
 import math
+import smbus
+import json
 
 
 SPEED_OF_LIGHT = 299792458
+I2C_MAX_CHUNK_LIMIT = 32
+I2C_START_REGISTER = 0x59
+I2C_BLOCK_LENGTH = 9
 
 
 class LidarSensor:
     def __init__(self, slave_address):
         self.slave_address = slave_address
+        
+        # default values
         self.measured_time = 0
         self.frequency = 0
+        self.angle = 0
+
+        # calculates values
+        self.data_raw = None
+        self.distance = 0
+        self.diameter = 0
+
 
     #
     # TF-Luna distance calculation
     # d = (c / 2) * (1 / 2 * pi * f) * d_phi
     #    
+    def calculate_distance():
+        self.distance = (SPEED_OF_LIGHT / 2) * (1 / 2*math.pi*self.frequency) * self.measured_time
+
+
+    def calculate_diameter():
+        self.diameter = 2 * self.distance * math.tan(self.angle)
+
+
     def get_distance():
-        return (SPEED_OF_LIGHT / 2) * (1 / 2*math.pi*self.frequency) * self.measured_time
+        return self.distance
 
 
     def get_raw_data():
-        pass
+        return self.data_raw
 
     #
     # TF-Luna communication protocol:
     # - I2C
     #
-    def read_sensor():
+    def read_data(bus, slave_address):
+        buff = {}
+
+        try:
+            bus.write_byte(slave_address, 0x01)
+            block = bus.read_i2c_block_data(slave, I2C_START_REGISTER, I2C_BLOCK_LENGTH)
+            
+            if block[0] == 0x59 and block[1] == 0x59:
+                buff = {
+                    "distance": block[2] + block[3] * 256,
+                    "strength": block[4] + block[5] * 256,
+                    "temperature": block[6] + block[7] * 256
+                }
+            else:
+                rospy.logerror("Can't read the data")
+
+        except Exception as e:
+            rospy.logerror(f"{e}")
+
+        return buff
+
+
+    def send_data(bus, slave_address, buff):
+        pass
+
+
+    def get_firmware_version():
         pass
--- src/ti_lidar_sensor_node.py
+++ src/ti_lidar_sensor_node.py
@@ -2,12 +2,14 @@
 
 
 import rospy
+import ti_lidar_sensor as tls
 
 
 def main():
     rospy.init_node("ti_lidar_sensor")
     rospy.loginfo("Yo mama")
-
+    
+    lidar_sensor = LidarSensor(0x)
 
     rospy.spin()
 
